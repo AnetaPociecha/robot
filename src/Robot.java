ba9476e5a3b825e8f236274b51761157f6781db5
apociecha <34507866+apociecha@users.noreply.github.com>
2017-12-13 16:29:01 +0100

    Update Robot.java

--- src/Robot.java
+++ src/Robot.java
@@ -1,9 +1,27 @@
 public class Robot {
-    int x;
-    int y;
 
-    double readSignal(Antena antena){
-        return antena.getSignal(x,y);
+    private int xR;
+    private int yR;
+
+    public Robot(int xR, int yR) {
+        this.xR=xR;
+        this.yR=yR;
     }
 
+    double readSignal(Antena antena){
+        return antena.getSignal(xR,yR);
+    }
+
+    public void moveRight() {
+        xR++;
+    }
+    public void moveLeft() {
+        if(xR!=0) xR--;
+    }
+    public void moveForward() {
+        yR++;
+    }
+    public void moveBack() {
+        if(yR!=0) yR--;
+    }
 }
