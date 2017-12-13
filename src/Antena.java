0efbb0594e1d31472ec11fadb78c49b0b2c7d514
apociecha <34507866+apociecha@users.noreply.github.com>
2017-12-13 16:28:25 +0100

    Update Antena.java

--- src/Antena.java
+++ src/Antena.java
@@ -1,15 +1,19 @@
 import java.lang.*;
 
-
 public class Antena {
     final private int A = 100;
     final private int n = 1;
-    int x;
-    int y;
+    private int xA;
+    private int yA;
+
+    public Antena(int xA, int yA) {
+        this.xA=xA;
+        this.yA=yA;
+    }
 
     double getSignal(int xR,int yR){
         double distance;
-        distance=Math.sqrt(Math.pow(xR-x,2)+Math.pow(yR-y,2));
+        distance=Math.sqrt(Math.pow(xR-xA,2)+Math.pow(yR-yA,2));
         double RSS = A-10*n*Math.log(distance);
         return RSS;
     } //edit sth ale czad
